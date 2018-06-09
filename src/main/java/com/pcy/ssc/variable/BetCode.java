04e47426b01679c23a6b1ecdd903cf67d85ea93b
a9250 <a9250@DESKTOP-8J99G0E>
2018-06-09 16:36:19 +0800

    w

--- src/main/java/com/pcy/ssc/variable/BetCode.java
+++ src/main/java/com/pcy/ssc/variable/BetCode.java
@@ -1,19 +1,19 @@
 package com.pcy.ssc.variable;
 
 public class BetCode {
-	public static final String Q1_MAX="newip_1-1005";
-	public static final String Q1_MIN="newip_1-1006";
-	public static final String Q2_MAX="newip_2-1005";
-	public static final String Q2_MIN="newip_2-1006";
-	public static final String Q3_MAX="newip_3-1005";
-	public static final String Q3_MIN="newip_3-1006";
-	public static final String Q4_MAX="newip_4-1005";
-	public static final String Q4_MIN="newip_4-1006";
-	public static final String Q5_MAX="newip_5-1006";
-	public static final String Q5_MIN="newip_5-1005";
+	public static final String Q1_MAX="newip_1-1105";
+	public static final String Q1_MIN="newip_1-1106";
+	public static final String Q2_MAX="newip_2-1105";
+	public static final String Q2_MIN="newip_2-1106";
+	public static final String Q3_MAX="newip_3-1105";
+	public static final String Q3_MIN="newip_3-1106";
+	public static final String Q4_MAX="newip_4-1105";
+	public static final String Q4_MIN="newip_4-1106";
+	public static final String Q5_MAX="newip_5-1106";
+	public static final String Q5_MIN="newip_5-1105";
 	//单双
-	public static final String Q1_DAN="newip_1-1007";
-	public static final String Q1_SHUANG="newip_1-1008";
+	public static final String Q1_DAN="newip_1-1107";
+	public static final String Q1_SHUANG="newip_1-1108";
 	//总大小
 	public static final String TOTAL_MAX="newip_1109";
 	public static final String TOTAL_MIN="newip_1110";
